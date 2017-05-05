14bbb229a6e494061b01113c26fd9cebf37f4e34
CK110 <747272975@qq.com>
2017-05-05 13:42:07 +0800

    build-lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     InputTextModule,
     DialogModule,
     BrowserAnimationsModule,
-    ButtonModule
+    ButtonModule,
   ],
   providers: [
     // ValidateMsgService
